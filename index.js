4fe83ffa4e218d1e05326c268c93efbfd7ae6635
dukebao <steven.chang420@gmail.com>
2021-05-06 21:20:54 -0700

    change line 50 on index to make sure it links to animelist

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get('/auth/google/callback',
     passport.authenticate('google', { failureRedirect: '/login' }),
     function(req, res) {
         // Successful authentication, redirect home.
-        res.redirect('/animelist');
+        res.redirect('/list');
     });
 app.post('/auth/google', authController.googleSubmit);
 
