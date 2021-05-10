aa661b3235bc3a5a9cfeb1fdc2a80ef345adfbe7
bleulenny <alex.amog@hotmail.com>
2021-05-10 00:00:08 -0700

    Commented out sqlite_anime. Needs to be reworked.

--- sqlite_anime.js
+++ sqlite_anime.js
@@ -1,37 +1,37 @@
-const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('./database/anime.db');
+// const sqlite3 = require('sqlite3').verbose();
+// let db = new sqlite3.Database('./database/anime.db');
 
-let get_anime_list = () => {
-    db.each(`SELECT * FROM Anime_Shows`, function(err, row) {
-        console.log(`ID:${row.ANI_ID} Anime Name: ${row.ANI_Title} Anime DESC: ${row.ANI_Desc} Anime Pic: ${row.ANI_Pic}`);
-    });
-};
+// let get_anime_list = () => {
+//     db.each(`SELECT * FROM Anime_Shows`, function(err, row) {
+//         console.log(`ID:${row.ANI_ID} Anime Name: ${row.ANI_Title} Anime DESC: ${row.ANI_Desc} Anime Pic: ${row.ANI_Pic}`);
+//     });
+// };
 
-let add_anime = (anime_title, anime_desc, anime_pic) => {
-    db.run('INSERT INTO Anime_Shows VALUES(NULL,?,?,?);', [anime_title, anime_desc, anime_pic], function(err) {
-        if (err) {
-            return err;
-        }
-    });
-};
+// let add_anime = (anime_title, anime_desc, anime_pic) => {
+//     db.run('INSERT INTO Anime_Shows VALUES(NULL,?,?,?);', [anime_title, anime_desc, anime_pic], function(err) {
+//         if (err) {
+//             return err;
+//         }
+//     });
+// };
 
-let remove_anime = (anime_id) => {
-    db.run(`DELETE FROM Anime_Shows WHERE ANI_ID = ${anime_id}`, function(err) {
-        if (err) {
-            return err;
-        };
-    });
-}
+// let remove_anime = (anime_id) => {
+//     db.run(`DELETE FROM Anime_Shows WHERE ANI_ID = ${anime_id}`, function(err) {
+//         if (err) {
+//             return err;
+//         };
+//     });
+// }
 
 
-add_anime('Naruto', 'Ninja', 'dwehfwjef');
+// add_anime('Naruto', 'Ninja', 'dwehfwjef');
 
-get_anime_list();
+// get_anime_list();
 
-db.close();
+// db.close();
 
-module.exports = {
-    get_anime_list,
-    add_anime,
-    remove_anime
-};
+// module.exports = {
+//     get_anime_list,
+//     add_anime,
+//     remove_anime
+// };
